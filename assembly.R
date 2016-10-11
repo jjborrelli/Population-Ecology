0e9ad5ee8d83ea705a32ec41d16dc38966ffd257
Jon Borrelli <borrejj@gmail.com>
2016-10-10 23:39:29 -0400

    start an assembly script

---
+++ assembly.R
@@ -0,0 +1,108 @@
+library(igraph)
+library(igraphdata)
+library(deSolve)
+library(rend)
+library(NetIndices)
+library(reshape2)
+
+motif_counter <- function(graph.lists){
+  require(igraph)
+  
+  if(!is.list(graph.lists)){
+    stop("The input should be a list of graph objects")
+  }
+  
+  triad.count <- lapply(graph.lists, triad.census)
+  triad.matrix <- matrix(unlist(triad.count), nrow = length(graph.lists), ncol = 16, byrow = T)
+  colnames(triad.matrix) <- c("empty", "single", "mutual", "s5", "s4", "s1", "d4",
+                              "d3", "s2", "s3","d8", "d2", "d1", "d5", "d7", "d6")
+  
+  triad.df <- as.data.frame(triad.matrix)
+  
+  motif.data.frame <- data.frame(s1 = triad.df$s1, s2 = triad.df$s2, s3 = triad.df$s3, s4 = triad.df$s4, 
+                                 s5 = triad.df$s5, d1 = triad.df$d1, d2 = triad.df$d2, d3 = triad.df$d3, d4 = triad.df$d4,
+                                 d5 = triad.df$d5, d6 = triad.df$d6, d7 = triad.df$d7, d8 = triad.df$d8)
+  
+  return(motif.data.frame)
+}
+
+getmot <- function(web){
+  com <- combn(length(V(web)), 3)
+  adj <- get.adjacency(web, sparse = F)
+  
+  tbt <- lapply(1:ncol(com), function(x){adj[com[,x], com[,x]]})
+  conn <- sapply(tbt, function(x) is.connected(graph.adjacency(x)))
+  mots <- motif_counter(lapply(tbt[conn], graph.adjacency))
+  
+  m1 <- melt(lapply(1:sum(conn), function(x) com[,x]))
+  m2 <- mots[m1$L1,]
+  m3 <- aggregate(m2, list(m1$value), sum)
+  
+  
+  return(m3)
+}
+
+ext1 <- function(times, states, parms){
+  with(as.list(states),{
+    states[states < (10^(-10))] <- 0
+    return(c(states))
+  })
+}
+
+n.vals <- function(S,C){
+  niche<-runif(S,0,1)
+  r<-rbeta(S,1,((1/(2*C))-1))*niche
+  
+  ci <- runif(S, r/2, niche)
+  
+  return(cbind(niche = niche, ci = ci, r = r))
+}
+
+n.mat <- function(nv){
+  S <- nrow(nv)
+  new.mat<-matrix(0,nrow=S,ncol=S)
+  
+  for(i in 1:S){
+    
+    for(j in 1:S){
+      if(nv[j,1]>(nv[i,2]-(.5*nv[i,3])) && nv[j,1]<(nv[i,2]+.5*nv[i,3])){
+        new.mat[j,i]<-1
+      }
+    }
+  }
+  
+  #new.mat<-new.mat[order(apply(new.mat,2,sum)),order(apply(new.mat,2,sum))]
+  return(new.mat)
+}
+
+cond <- FALSE
+while(!cond){
+  nva <- n.vals(100, .15)
+  nm1 <- n.mat(nva)
+  
+  cond <- is.connected(graph.adjacency(nm1))
+}
+
+tind <- TrophInd(nm1)
+xi <- (((10^2)^tind$TL)/100)^-0.25*0.314
+ri <- as.numeric(colSums(nm1) == 0)
+
+spp1 <- 1:100
+par <- list(K = 1, x.i = xi[spp1], yij = 8, eij = 0.85, xpar = 0.2, 
+            B.o = 0.5, r.i = ri[spp1], A = nm1[spp1, spp1], G.i = Gi, FR = Fij)
+
+out1 <- ode(y = runif(100, .1, 1), times = 1:2000, func = CRmod, parms = par, 
+            events = list(func = ext1, time = 1:2000))
+matplot(out1[,-1], typ = "l")
+matplot(out1[1900:2000,-1], typ = "l")
+sd(out1[1800:2000,-1])/mean(out1[1800:2000,-1])
+
+cv1 <- apply(out1[,-1], 2, function(x) sd(x[1800:2000])/mean(x[1800:2000]))
+cv1[is.nan(cv1)] <- 0
+
+plot(cv1~tind$TL)
+tind2 <- TrophInd(nm1[out1[2000,-1] > 0, out1[2000,-1] > 0])
+plot(log(cv1[out1[2000,-1] > 0])~tind2$TL)
+
+
+mot1 <- getmot(graph.adjacency(nm1))
